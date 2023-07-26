31955217884956db18d7d0c0ab2de756b3552730
hadizzo <masimo.hadizzo@gmail.com>
2023-07-26 13:58:48 +0330

    main page changed

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -1,9 +1,52 @@
-import React from "react";
-import { FaMusic } from "react-icons/fa";
+import React, { useState, useRef } from "react";
+import {
+  FaMusic,
+  FaPlay,
+  FaPause,
+  FaEdit,
+  FaSave,
+  FaTimes,
+} from "react-icons/fa";
 import useWaveformLoader from "./useWaveformLoader";
 
 const MusicItem = ({ music }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(music.title);
+  const [editedArtist, setEditedArtist] = useState(music.artist);
+  const [editedGenre, setEditedGenre] = useState(music.genre);
+  const audioRef = useRef(null);
   const waveformRef = useWaveformLoader(music.audioSrc);
+
+  const handlePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying((prevState) => !prevState);
+    }
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    // Add your logic here to save the edited data.
+    setIsEditing(false);
+    // You can update the music item data in your state or backend here.
+  };
+
+  const handleCancel = () => {
+    // Discard the changes and exit edit mode.
+    setIsEditing(false);
+    // You can reset the edited data here if needed.
+    setEditedTitle(music.title);
+    setEditedArtist(music.artist);
+    setEditedGenre(music.genre);
+  };
 
   return (
     <li
@@ -17,20 +60,66 @@ const MusicItem = ({ music }) => {
     >
       <div style={{ display: "flex", alignItems: "center", width: "150px" }}>
         <FaMusic size={20} style={{ marginRight: "0.5rem" }} />
-        <strong>{music.title}</strong>
+        {isEditing ? (
+          <input
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+        ) : (
+          <strong>{music.title}</strong>
+        )}
       </div>
       <p style={{ width: "150px" }}>
         <span style={{ marginRight: "1rem" }}>Artist:</span>
-        {music.artist}
+        {isEditing ? (
+          <input
+            value={editedArtist}
+            onChange={(e) => setEditedArtist(e.target.value)}
+          />
+        ) : (
+          music.artist
+        )}
       </p>
       <p style={{ width: "150px" }}>
         <span style={{ marginRight: "1rem" }}>Genre:</span>
-        {music.genre}
+        {isEditing ? (
+          <input
+            value={editedGenre}
+            onChange={(e) => setEditedGenre(e.target.value)}
+          />
+        ) : (
+          music.genre
+        )}
       </p>
       <div
         ref={waveformRef}
         style={{ flex: 1, marginLeft: "1rem", height: "150px" }} // Adjust height as needed
       />
+      <div style={{ marginLeft: "1rem" }}>
+        {isEditing ? (
+          <>
+            <button onClick={handleSave} title="Save">
+              <FaSave />
+            </button>
+            <button onClick={handleCancel} title="Cancel">
+              <FaTimes />
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={handlePlayPause}
+              title={isPlaying ? "Pause" : "Play"}
+            >
+              {isPlaying ? <FaPause /> : <FaPlay />}
+            </button>
+            <button onClick={handleEdit} title="Edit">
+              <FaEdit />
+            </button>
+          </>
+        )}
+      </div>
+      <audio ref={audioRef} src={music.audioSrc} />
     </li>
   );
 };
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,20 +1,109 @@
 import React from "react";
+import { FaMusic, FaGuitar, FaMicrophone, FaEdit } from "react-icons/fa";
+import MusicItem from "./MusicItem";
 
 const UserProfile = () => {
-  // Fetch user information here (e.g., picture, name)
+  // Sample data for the user's music collection
+  const userMusicCollection = [
+    {
+      id: 1,
+      title: "Song 1",
+      artist: "Artist 1",
+      genre: "Genre 1",
+      audioSrc: "/path/to/audio1.mp3",
+    },
+    {
+      id: 2,
+      title: "Song 2",
+      artist: "Artist 2",
+      genre: "Genre 2",
+      audioSrc: "/path/to/audio2.mp3",
+    },
+    // Add more music data as needed
+  ];
+
+  // Sample user information
   const user = {
-    name: "John Doe",
-    picture: "profilepic.jpg",
+    name: "Hadi Masoumi",
+    bio: "Music lover and enthusiast. Follow me for more great tunes!",
+    followers: 1000,
+    socialMedia: {
+      twitter: "https://twitter.com/johndoe",
+      instagram: "https://instagram.com/johndoe",
+      facebook: "https://facebook.com/johndoe",
+    },
   };
 
   return (
-    <div
-      style={{ backgroundColor: "#445566", padding: "1rem", color: "white" }}
-    >
-      <img src={user.picture} alt="User" />
-      <h3>{user.name}</h3>
-      {/* Add other user profile information */}
-      {/* e.g., Email, Age, etc. */}
+    <div style={{ display: "flex" }}>
+      {/* Render the user's profile picture */}
+      {/* <img
+        src="./profilepic.jpg"
+        alt="Profile"
+        style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+      /> */}
+
+      <div style={{ marginLeft: "1rem" }}>
+        {/* Render user information */}
+        <h2>{user.name}</h2>
+        <p>{user.bio}</p>
+        <p>Followers: {user.followers}</p>
+
+        {/* Render social media links */}
+        <div>
+          <a
+            href={user.socialMedia.twitter}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href={user.socialMedia.instagram}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href={user.socialMedia.facebook}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+        </div>
+
+        {/* Additional features */}
+        <div style={{ marginTop: "1rem" }}>
+          <h3>Additional Features</h3>
+          <ul>
+            <li>
+              <FaMusic size={20} style={{ marginRight: "0.5rem" }} />
+              Piano stuff
+            </li>
+            <li>
+              <FaGuitar size={20} style={{ marginRight: "0.5rem" }} />
+              Guitar stuff
+            </li>
+            <li>
+              <FaMicrophone size={20} style={{ marginRight: "0.5rem" }} />
+              Compositing stuff
+            </li>
+            <li>
+              <FaEdit size={20} style={{ marginRight: "0.5rem" }} />
+              Editing stuff
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Render the user's music collection */}
+      <ul>
+        {/* {userMusicCollection.map((music) => (
+          <MusicItem key={music.id} music={music} />
+        ))} */}
+      </ul>
     </div>
   );
 };
