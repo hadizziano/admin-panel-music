0857c6323ba6903e045a98f3544a1d55a34101a1
hadizzo <masimo.hadizzo@gmail.com>
2023-07-28 21:51:34 +0330

    piano stuff is completed

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -34,7 +34,7 @@ const Piano = () => {
 
   const playNote = (note) => {
     const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease(note, "0.1"); // Play the note for 0.5 seconds
+    synth.triggerAttackRelease(note, "0.1"); // Play the note for 0.1 seconds
   };
 
   useEffect(() => {
@@ -48,26 +48,61 @@ const Piano = () => {
   }, [currentKey]);
 
   const pianoKeys = [
-    { key: "Q", note: "C4" },
-    { key: "2", note: "C#4" },
-    { key: "W", note: "D4" },
-    { key: "3", note: "D#4" },
-    { key: "E", note: "E4" },
-    { key: "R", note: "F4" },
-    { key: "5", note: "F#4" },
-    { key: "T", note: "G4" },
-    { key: "6", note: "G#4" },
-    { key: "Y", note: "A4" },
-    { key: "7", note: "A#4" },
-    { key: "U", note: "B4" },
+    { key: "A", note: "C3", isBlack: false },
+    { key: "W", note: "C#3", isBlack: true },
+    { key: "S", note: "D3", isBlack: false },
+    { key: "E", note: "D#3", isBlack: true },
+    { key: "D", note: "E3", isBlack: false },
+    { key: "F", note: "F3", isBlack: false },
+    { key: "T", note: "F#3", isBlack: true },
+    { key: "G", note: "G3", isBlack: false },
+    { key: "Y", note: "G#3", isBlack: true },
+    { key: "H", note: "A3", isBlack: false },
+    { key: "U", note: "A#3", isBlack: true },
+    { key: "J", note: "B3", isBlack: false },
+    { key: "K", note: "C4", isBlack: false },
+    { key: "O", note: "C#4", isBlack: true },
+    { key: "L", note: "D4", isBlack: false },
+    { key: "P", note: "D#4", isBlack: true },
+    { key: ";", note: "E4", isBlack: false },
+    { key: "'", note: "F4", isBlack: false },
+    { key: "]", note: "F#4", isBlack: true },
+    { key: "\\", note: "G4", isBlack: false },
+    { key: "Z", note: "G#4", isBlack: true },
+    { key: "X", note: "A4", isBlack: false },
+    { key: "D", note: "A#4", isBlack: true },
+    { key: "C", note: "B4", isBlack: false },
+    { key: "V", note: "C5", isBlack: false },
+    { key: "G", note: "C#5", isBlack: true },
+    { key: "B", note: "D5", isBlack: false },
+    { key: "H", note: "D#5", isBlack: true },
+    { key: "N", note: "E5", isBlack: false },
+    { key: "M", note: "F5", isBlack: false },
+    { key: ",", note: "F#5", isBlack: true },
+    { key: ".", note: "G5", isBlack: false },
+    { key: "/", note: "G#5", isBlack: true },
+    { key: "Q", note: "A5", isBlack: false },
+    { key: "2", note: "A#5", isBlack: true },
+    { key: "W", note: "B5", isBlack: false },
+    { key: "E", note: "C6", isBlack: false },
+    { key: "4", note: "C#6", isBlack: true },
+    { key: "R", note: "D6", isBlack: false },
+    { key: "5", note: "D#6", isBlack: true },
+    { key: "T", note: "E6", isBlack: false },
   ];
 
   const activeKeyStyle = {
     backgroundColor: "#ffb347", // Active key color
+    marginTop: "0px",
   };
 
-  const inactiveKeyStyle = {
-    backgroundColor: "#fff", // Inactive key color
+  const inactiveWhiteKeyStyle = {
+    backgroundColor: "#fff", // Inactive white key color
+  };
+
+  const inactiveBlackKeyStyle = {
+    backgroundColor: "#000", // Inactive black key color
+    marginTop: "0px", // Adjust the marginTop value as needed
   };
 
   const renderPianoKeys = () => {
@@ -75,8 +110,14 @@ const Piano = () => {
       <div
         key={keyObj.key}
         id={keyObj.note}
-        className="piano-key"
-        style={currentKey === keyObj.key ? activeKeyStyle : inactiveKeyStyle}
+        className={`piano-key ${keyObj.isBlack ? "black-key" : ""}`}
+        style={
+          currentKey === keyObj.key
+            ? activeKeyStyle
+            : keyObj.isBlack
+            ? inactiveBlackKeyStyle
+            : inactiveWhiteKeyStyle
+        }
         onMouseDown={() => handleMouseDown(keyObj.note)}
         onMouseUp={handleMouseUp}
       />
